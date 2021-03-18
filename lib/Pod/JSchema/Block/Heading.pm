6318b9acebb933aa9f79a9eb8bab464e47836c99
Justin Schell <justin@gudtech.com>
2021-03-18 12:10:21 -0700

    Fix warnings

--- lib/Pod/JSchema/Block/Heading.pm
+++ lib/Pod/JSchema/Block/Heading.pm
@@ -14,8 +14,8 @@ sub _parse{
     my $pkg  = shift;
     my $tag  = shift;
     my $text = shift;
-    
-    
+
+
     my ($level) = $tag =~ /(\d+)/;
     my ($title) = $text =~ /^(.*?)[\n\r]/;
     return __PACKAGE__->new ( title => $title, tag => $tag, level => $level );
@@ -23,25 +23,25 @@ sub _parse{
 
 sub markdown{
     my $self = shift;
-    
+
     my $out;
     $out .= "#" . ("#" x $self->level ) . ' ' .  $self->title . "\n";
-    
+
     $out .= $self->body;
-    
+
     return $out;
 }
 
 sub html{
     my $self = shift;
-    
+
     my $out;
     my $tag = $self->tag;
     $out .= qq!<div class="block $tag block-$tag">\n!;
-    $out .= qq!<div class="heading">! . encode_entities( $self->title ) . "</div>\n";
-    my $body = encode_entities($self->body);
+    $out .= qq!<div class="heading">! . (encode_entities($self->title) // '') . "</div>\n";
+    my $body = encode_entities($self->body) // '';
     $body =~ s/  \n/<br\/>/g; #cheat it with markdown style linebreaks
-    
+
     $out .= qq'<div class="section">$body</div>\n';
     $out .= "</div> <!-- end block -->\n";
     return $out;
